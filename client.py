01e937de1bd423dde0ba8f318cc0d4f614916ba0
ixmjk <amirakb79@gmail.com>
2022-12-05 19:07:49 +0330

    feat: add server.py and client.py

---
+++ client.py
@@ -0,0 +1,96 @@
+import socket
+import json
+import os
+import base64
+
+
+FORMAT = 'utf-8'
+
+
+class Client:
+    def __init__(self, ip, port) -> None:
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((ip, port))
+
+    def send(self, data):
+        json_data = json.dumps(data)
+        self.client.send(json_data.encode(FORMAT))
+
+    def recv(self):
+        json_data = ''
+        while True:
+            try:
+                json_data = json_data + self.client.recv(1024).decode(FORMAT)
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
+    def write_file(self, filename, content):
+        # filename = f'[upload {datetime.datetime.now()}] {filename}'
+        with open(filename, 'wb') as file:
+            file.write(base64.b64decode(content))
+            return f'[+] {filename} downloaded successfully.'
+
+    def read_file(self, path):
+        with open(path, 'rb') as file:
+            return base64.b64encode(file.read()).decode(FORMAT)
+
+    def uploadable(self, file_path):
+        if os.path.isfile(file_path):
+            return True
+        if os.path.isfile(os.path.join(os.path.dirname(__file__), file_path)):
+            return True
+        return False
+
+    def run(self):
+        while True:
+            try:
+                self.send('info')
+                info = self.recv()
+                command = input(f'{info}# ').split(' ')
+                if command[0] == 'exit':
+                    self.send('exit')
+                    exit()
+                elif command[0] == 'clear':
+                    def clear(): return os.system('cls' if os.name == 'nt' else 'clear')
+                    clear()
+                elif command[0] == 'download':
+                    file_path = ' '.join(command[1:])
+                    self.send(f'downloadable {file_path}')
+                    downloadable = self.recv()
+                    if downloadable:
+                        file_name = os.path.basename(file_path)
+                        self.send(' '.join(command))
+                        file_content = self.recv().encode(FORMAT)
+                        print(self.write_file(file_name, file_content))
+                    else:
+                        print(f'download: {file_path}: Not downloadable')
+                elif command[0] == 'upload':
+                    file_path = ' '.join(command[1:])
+                    if self.uploadable(file_path):
+                        file_name = os.path.basename(file_path)
+                        file_content = self.read_file(file_path)
+                        self.send(f'upload {file_name}')
+                        self.send(file_content)
+                        print(self.recv())
+                    else:
+                        print(f'upload: {file_path}: Not uploadable')
+                else:
+                    command = ' '.join(command)
+                    self.send(command)
+                    result = self.recv()
+                    print(result)
+            except Exception as e:
+                print(str(e))
+            except KeyboardInterrupt:
+                pass
+
+
+def main():
+    SERVER = socket.gethostbyname(socket.gethostname())
+    my_client = Client(SERVER, 4444)
+    my_client.run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ server.py
@@ -0,0 +1,115 @@
+import socket
+import subprocess
+import json
+import os
+import base64
+import getpass
+
+
+FORMAT = 'utf-8'
+
+
+class Server:
+    def __init__(self) -> None:
+        self.ip = socket.gethostbyname(socket.gethostname())
+        self.port = 4444
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server.bind((self.ip, self.port))
+        server.listen(0)
+        print(f'[+] Listening on port {self.port}...')
+        self.connection, self.address = server.accept()
+        print(f'[+] {self.address} connected.')
+
+    @staticmethod
+    def execute_system_command(command):
+        result = subprocess.getoutput(command)
+        return result
+
+    def send(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data.encode(FORMAT))
+
+    def recv(self):
+        json_data = ''
+        while True:
+            try:
+                json_data = json_data + self.connection.recv(1024).decode(FORMAT)
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
+    def pwd(self):
+        return os.getcwd()
+
+    def cd(self, path):
+        try:
+            os.chdir(path)
+            return ''
+        except FileNotFoundError:
+            return f'cd: {path}: No such file or directory'
+
+    def ls(self):
+        return '  '.join(os.listdir())
+
+    def read_file(self, path):
+        with open(path, 'rb') as file:
+            return base64.b64encode(file.read()).decode(FORMAT)
+
+    def write_file(self, filename, content):
+        # filename = f'[upload {datetime.datetime.now()}] {filename}'
+        with open(filename, 'wb') as file:
+            file.write(base64.b64decode(content))
+            return f'[+] {filename} uploaded successfully.'
+
+    def downloadable(self, file_path):
+        if os.path.isfile(file_path):
+            return True
+        if os.path.isfile(os.path.join(os.path.dirname(__file__), file_path)):
+            return True
+        return False
+
+    def run(self):
+        while True:
+            try:
+                command = self.recv().split(' ')
+                if command[0] == 'exit':
+                    print(f'[-] {self.address} disconnected.')
+                    self.__init__()
+                elif command[0] == 'info' and len(command) == 1:
+                    self.send(f'{getpass.getuser()}@{socket.gethostname()}:{os.getcwd()}')
+                elif command[0] == 'pwd' and len(command) == 1:
+                    self.send(self.pwd())
+                elif command[0] == 'cd' and len(command) > 1:
+                    self.send(self.cd(' '.join(command[1:])))
+                elif command[0] == 'ls' and len(command) == 1:
+                    self.send(self.ls())
+                elif command[0] == 'download' and len(command) > 1:
+                    file_path = ' '.join(command[1:])
+                    file_content = self.read_file(file_path)
+                    self.send(file_content)
+                elif command[0] == 'downloadable' and len(command) > 1:
+                    file_path = ' '.join(command[1:])
+                    self.send(self.downloadable(file_path))
+                elif command[0] == 'upload' and len(command) > 1:
+                    file_name = ' '.join(command[1:])
+                    file_content = self.recv().encode(FORMAT)
+                    self.send(self.write_file(file_name, file_content))
+                else:
+                    command = ' '.join(command)
+                    command_result = self.execute_system_command(command)
+                    self.send(command_result)
+            except ConnectionResetError:
+                print(f'[-] {self.address} disconnected.')
+                self.__init__()
+            except Exception as e:
+                self.send(str(e))
+
+
+def main():
+    my_server = Server()
+    my_server.run()
+
+
+if __name__ == '__main__':
+    main()
